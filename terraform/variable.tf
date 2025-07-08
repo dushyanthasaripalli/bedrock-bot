79713a5d1d242edfb680170b5433016e1d8dafce
dushyanthasaripalli <dushyanthasaripalli@gmail.com>
2025-07-07 19:25:29 -0500

    commented code

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,19 +1,19 @@
-variable ou{}
-variable acl{}
-variable tags{}
-variable region{}
-variable account_id{}
+# variable ou{}
+# variable acl{}
+# variable tags{}
+# variable region{}
+# variable account_id{}
 
-variable fb_scripts_bucket{}
-variable fb_results_bucket{}
-variable fb_adhoc_bucket{}
+# variable fb_scripts_bucket{}
+# variable fb_results_bucket{}
+# variable fb_adhoc_bucket{}
 
-variable cappy_ai_buckets{}
-variable fb_scripts_bucket_files{}
+# variable cappy_ai_buckets{}
+# variable fb_scripts_bucket_files{}
 
-variable glueetl_rds_role_arn{}
-variable rs_account_id_clm{}
-variable rs_cluster_id_clm{}
-variable rs_db_clm{}
-variable rs_db_user_clm{}
-variable rs_edla_role_clm{}
+# variable glueetl_rds_role_arn{}
+# variable rs_account_id_clm{}
+# variable rs_cluster_id_clm{}
+# variable rs_db_clm{}
+# variable rs_db_user_clm{}
+# variable rs_edla_role_clm{}
